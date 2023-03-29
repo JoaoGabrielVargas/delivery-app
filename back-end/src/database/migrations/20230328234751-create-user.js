459f051cace8a003b46cfff589a98115d090ca0a
Mychel Albuquerque <mychelalbu@gmail.com>
2023-03-28 21:35:35 -0300

    feat: configura sequelize

---
+++ back-end/src/database/migrations/20230328234751-create-user.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+      name: {
+        type: Sequelize.STRING },
+      role: {
+        type: Sequelize.STRING },
+      email: {
+        type: Sequelize.STRING },
+      password: {
+        type: Sequelize.STRING },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
