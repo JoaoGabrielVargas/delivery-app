c7df5523b0202b9a000842f5220821ee518c20d0
Joao Gabriel Vargas <joao.gaiares@gmail.com>
2023-03-30 19:37:58 -0300

    feat: navbar criada para o header

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const LoginRouter = require('../routes/LoginRouter');
+  
+const app = express(); 
+app.use(cors()); 
 
-const app = express();
-app.use(cors());
-
-app.use(express.json());
+app.use(express.json()); 
 
 app.use('/login', LoginRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
